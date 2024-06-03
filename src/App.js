3d8ced5ea26e2b6f71d89072789eb2a11b20aa1e
AARUSH SABOO <160993404+aarushsaboo@users.noreply.github.com>
2024-06-03 04:17:37 +0000

    added formatting too

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
     "initial-investment": 10000,
     "annual-investment": 300,
     "expected-return": 5.5,
-    duration: 12
+    duration: 12,
   });
   function handleInputChange(name, value) {
     setFormValues((prevValues) => {
@@ -19,16 +19,22 @@ function App() {
       };
     });
   }
+  const inputIsValid = formValues["duration"] >= 1;
   return (
     <>
       <Header />
       <Inputs formValues={formValues} handleInputChange={handleInputChange} />
-      <Result
-        initialInvestment={formValues["initial-investment"]}
-        annualInvestment={formValues["annual-investment"]}
-        expectedReturn={formValues["expected-return"]}
-        duration={formValues["duration"]}
-      />
+      {inputIsValid && (
+        <Result
+          initialInvestment={formValues["initial-investment"]}
+          annualInvestment={formValues["annual-investment"]}
+          expectedReturn={formValues["expected-return"]}
+          duration={formValues["duration"]}
+        />
+      )}
+      {!inputIsValid && (
+        <p className="center">Please enter a duration greater than 0</p>
+      )}
     </>
   );
 }
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,9 @@
-import myLogo from '../assets/investment-calculator-logo.png'
+import myLogo from "../assets/investment-calculator-logo.png";
 export default function Header() {
   return (
-    <header className="center">
+    <header id="header">
       <h1>React Investment Calculator</h1>
-      <img
-        src={myLogo}
-        alt="Logo of a money bag"
-      />
+      <img src={myLogo} alt="Logo of a money bag" />
     </header>
   );
 }
